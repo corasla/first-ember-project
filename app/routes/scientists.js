26d203e659b4b7c9b07bc44ff2a4837200a4cf14
corasla <milearzv@gmail.com>
2017-03-16 13:15:44 +0200

    Fix failing integration test

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return ['Ana Blandiana', 'CinSaFieOare?', 'Mae Jemison', 'Albert Hofmann']
+        return ['Ana Blandiana', 'CinSaFieOare?', 'Mae Jemison', 'Albert Hofmann'];
     }
 });
