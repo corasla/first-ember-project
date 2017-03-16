37f5800dae21b97ae3506d86c7ad886f482dac46
Razvan Milea <ram@ad.rms.intern>
2017-03-16 14:31:40 +0200

    Add links to main page Fix broken acceptance test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -29,7 +29,7 @@ test('should link to contact information', function(assert) {
   visit('/');
   click('a:contains("Contact")');
   andThen(function() {
-    assert.equal(currentURL, '/contact', 'should navigate to contact page');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact page');
   });
 });
 
