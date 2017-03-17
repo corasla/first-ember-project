faf694278ce9da174c657662576ce0080c6c1a17
corasla <milearzv@gmail.com>
2017-03-17 13:39:03 +0200

    Add rental-property-type helper

---
+++ app/helpers/rental-property-type.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([type]) {
+  return communityPropertyTypes.includes(type) ? 'Community' : 'Standalone';
+}
+
+export default Ember.Helper.helper(rentalPropertyType);
